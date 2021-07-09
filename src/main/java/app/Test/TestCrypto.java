850920ca6e55b439f1529d5ed49437960ebcbc27
phong <quocphong20022000@gmail.com>
2021-07-09 11:44:28 +0700

    phong

--- src/main/java/app/Test/TestCrypto.java
+++ src/main/java/app/Test/TestCrypto.java
@@ -1,4 +1,6 @@
-package app.gui.nguoidung;
+package app.Test;
+
+import java.util.*;
 
 import app.gui.Crypto;
 
@@ -6,14 +8,16 @@ public class TestCrypto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = "becauseditconmemetvailon";
+		System.out.println("Nhap thu gi do");
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
 		Crypto cr = new Crypto();
 		try {
-			System.out.println(a);
+			System.out.println("Original text: " + a);
 			String b = cr.setPass(a);
-			System.out.println("â" + b);
+			System.out.println("Crypted text: " + b);
 			String c = cr.getPass(b);
-			System.out.println(c);
+			System.out.println("Encrypted text: " + c);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
